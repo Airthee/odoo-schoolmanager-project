f2e3eef8f3521b45d74edd67f06e854673de1c52
Raphaël Tison <raphael.tison@hotmail.fr>
2018-04-02 09:53:20 +0200

    Ajout de la colonne classes dans la vueTree du professeur

--- models/students.py
+++ models/students.py
@@ -12,12 +12,16 @@ class student(models.Model):
 	firstname = fields.Char()
 	lastname = fields.Char()
 	birthdate = fields.Date()
-	age = fields.Integer(compute='_compute_age')
+	age = fields.Integer(compute='_compute_age', default=0)
 	class_id = fields.Many2one('schoolmanager.classe', string="Classe")
 
 	@api.depends('birthdate')
 	def _compute_age(self):
 		for record in self:
 			now = datetime.date.today()
-			birthdate = datetime.datetime.strptime(record.birthdate, '%Y-%m-%d').date()
-			record.age = (now-birthdate).days/365
+			try:
+				birthdate = datetime.datetime.strptime(record.birthdate, '%Y-%m-%d').date()
+				record.age = (now-birthdate).days/365
+			except TypeError:
+				record.age = 0
+				
